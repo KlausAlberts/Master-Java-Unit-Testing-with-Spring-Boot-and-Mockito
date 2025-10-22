c3777d529363a8ef2036417e060b51e2542f8211
Klaus Alberts <klaus.alberts@alberts-software-velbert.de>
2025-10-22 06:40:31 +0200

    return ResponseEntity instead of string

--- src/main/java/com/in28minutes/unittesting/controller/ItemController.java
+++ src/main/java/com/in28minutes/unittesting/controller/ItemController.java
@@ -3,33 +3,38 @@ package com.in28minutes.unittesting.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.in28minutes.unittesting.business.ItemBusinessService;
 import com.in28minutes.unittesting.model.Item;
 
 @RestController
+@RequestMapping("/item")
 public class ItemController {
 
 	@Autowired
 	private ItemBusinessService businessService;
 
 	@GetMapping("/dummy-item")
-	public Item dummyItem() {
-		return new Item(1, "Ball", 10, 100);
+	public ResponseEntity<Item> dummyItem() {
+		Item item = new Item(1, "Ball", 10, 100);
+		return new ResponseEntity<>(item, HttpStatus.OK);
 	}
 
 	@GetMapping("/item-from-business-service")
-	public Item itemFromBusinessService() {
+	public ResponseEntity<Item> itemFromBusinessService() {
 		Item item = businessService.retreiveHardcodedItem();
-
-		return item;
+		return new ResponseEntity<>(item, HttpStatus.OK);
 	}
 
 	@GetMapping("/all-items-from-database")
-	public List<Item> retrieveAllItems() {
-		return businessService.retrieveAllItems();
+	public ResponseEntity<List<Item>> retrieveAllItems() {
+		List<Item> items = businessService.retrieveAllItems();
+		return new ResponseEntity<>(items, HttpStatus.OK);
 	}
 
 }
